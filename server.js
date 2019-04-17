bb6a5d0ed2df699469e16974f1040afc6a304090
Jonathan <jonokwong@hotmail.com>
2019-04-16 20:43:40 -0700

    server package and gitignore

---
+++ server.js
@@ -0,0 +1,11 @@
+const  express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 8080;
+
+console.log("Final Round!");
+
+app.listen(port,() => {
+    console.log(`Server is up on the port ${port}`);
+});
